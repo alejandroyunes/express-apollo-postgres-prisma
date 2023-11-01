ceba62bfbdb9955c6d0af03eb35d4bded32cc72d
alejandro yunes <alejandroyunes@outlook.com>
2023-11-01 16:38:52 -0500

    adding the client to add to db

---
+++ src/script.ts
@@ -0,0 +1,29 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      email: 'jesus@waltmart.com',
+      username: 'jesus',
+    }
+  })
+  console.log(user)
+
+  const post = await prisma.post.create({
+    data: {
+      title: 'post jesus',
+      content: 'some content',
+      authorId: user.id,
+    }
+  })
+  console.log(post)
+}
+
+main().catch(e => console.error(e)).finally(async () => await prisma.$disconnect())
+
+/* exec": "ts-node ./src/index.ts" */
+
+
