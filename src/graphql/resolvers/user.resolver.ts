da95b7d0eb7ac9a42fb15fe7eb973cb7fe197276
alejandro yunes <alejandroyunes@outlook.com>
2023-11-02 02:51:09 -0500

    refoctor

--- src/graphql/resolvers/user.resolver.ts
+++ src/graphql/resolvers/user.resolver.ts
@@ -1,20 +1,14 @@
-import {GraphQLResolveInfo} from "graphql";
-import {createUser, getUser, getUsers} from "../services/user.service";
+import { GraphQLResolveInfo } from "graphql"
+import { createUser, getUser, getUsers } from "../services/user.service"
 
 export const usersResolver = {
   Query: {
-    async users(_: any, args: Record<string, any>, context: any, info: GraphQLResolveInfo) {
-      return await getUsers({info});
-    },
-    async user(_: any, args: Record<string, any>, context: any, info: GraphQLResolveInfo) {
-      return await getUser({id: args.id, info});
-    },
+    async users(_: any, args: Record<string, any>, context: any, info: GraphQLResolveInfo) { return await getUsers({ info }) },
+    async user(_: any, args: Record<string, any>, context: any, info: GraphQLResolveInfo) { return await getUser({ id: args.id, info }) }
   },
   Mutation: {
-    async createUser(_: any, {input}: Record<string, any>) {
-      return await createUser({email: input.email, username: input.username});
-    },
-    async updateUser() {},
-    async deleteUser() {},
-  },
-};
+    async createUser(_: any, { input }: Record<string, any>) { return await createUser({ email: input.email, username: input.username }) },
+    async updateUser() { },
+    async deleteUser() { },
+  }
+}
--- src/graphql/services/user.service.ts
+++ src/graphql/services/user.service.ts
@@ -1,6 +1,6 @@
-import {PrismaClient} from "@prisma/client"
-import {extractSelection} from "../utils/extractSelections"
-import {GraphQLResolveInfo} from "graphql"
+import { PrismaClient } from "@prisma/client"
+import { extractSelection } from "../utils/extractSelections"
+import { GraphQLResolveInfo } from "graphql"
 
 interface GetUsersArgs {
   info: GraphQLResolveInfo
@@ -17,30 +17,30 @@ interface UserInput {
 
 const prisma = new PrismaClient()
 
-export const getUsers = async ({info}: GetUsersArgs) => {
+export const getUsers = async ({ info }: GetUsersArgs) => {
   const extractedSelections = extractSelection(info)
   const postsIncluded = extractedSelections.includes("posts")
 
   if (postsIncluded) {
-    return await prisma.user.findMany({include: {posts: true}})
+    return await prisma.user.findMany({ include: { posts: true } })
   }
 
   return await prisma.user.findMany()
 }
 
-export const getUser = async ({id, info}: GetUserArgs) => {
+export const getUser = async ({ id, info }: GetUserArgs) => {
   const extractedSelections = extractSelection(info)
   const postsIncluded = extractedSelections.includes("posts")
 
   if (postsIncluded) {
-    return await prisma.user.findUnique({where: {id}, include: {posts: true}})
+    return await prisma.user.findUnique({ where: { id }, include: { posts: true } })
   }
 
-  return await prisma.user.findUnique({where: {id}})
+  return await prisma.user.findUnique({ where: { id } })
 }
 
-export const createUser = async ({email, username}: UserInput) => {
-  console.log({email, username})
+export const createUser = async ({ email, username }: UserInput) => {
+  console.log({ email, username })
 
   const createdUser = await prisma.user.create({
     data: {
